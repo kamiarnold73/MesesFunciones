8007ae6ac0f69ada4af389bbd87403940244eef5
kamiarnold73 <kamilla_arnold24@unikino.edu.mx>
2024-12-13 17:13:23 -0700

    completado

--- MesesFunciones/Program.cs
+++ MesesFunciones/Program.cs
@@ -4,34 +4,68 @@
     {
         static void Main(string[] args)
         {
-            MostrarMensaje("Hello, World!");
-            MostrarMensaje("Introducir un numero");
-            string mes = ObtenerMes(11);
-            MostrarMensaje (mes);
-        }
-
-        static void MostrarMensaje(string mensaje)
-        {
-            Console.WriteLine(mensaje);
-        }
-
-        static string ObtenerMes(int NumeroMes) 
-        {
-            switch (NumeroMes)
+            while (true)
             {
-                case 1: return "Enero";
-                case 2: return "Febrero";
-                case 3: return "Marzo";
-                case 4: return "Abril";
-                case 5: return "Mayo";
-                case 6: return "Junio";
-                case 7: return "Julio";
-                case 8: return "Agosto";
-                case 9: return "Septiembre";
-                case 10: return "Octubre";
-                case 11: return "Noviembre";
-                case 12: return "Diciembre";
-                default: return "Numero Incorrecto";
+                Console.WriteLine("Introducir un numero:");
+                string dato = Console.ReadLine();
+
+                int mes = 0;
+                bool sePudoConvertir = false;
+                sePudoConvertir = int.TryParse(dato, out mes);
+                if (sePudoConvertir)
+
+                {
+                    switch (mes)
+                    {
+                        case 1:
+                            Console.WriteLine("Enero");
+                            break;
+                        case 2:
+                            Console.WriteLine("Febrero");
+                            break;
+                        case 3:
+                            Console.WriteLine("Marzo");
+                            break;
+                        case 4:
+                            Console.WriteLine("Abril");
+                            break;
+                        case 5:
+                            Console.WriteLine("Mayo");
+                            break;
+                        case 6:
+                            Console.WriteLine("Junio");
+                            break;
+                        case 7:
+                            Console.WriteLine("Julio");
+                            break;
+                        case 8:
+                            Console.WriteLine("Agosto");
+                            break;
+                        case 9:
+                            Console.WriteLine("Septiembre");
+                            break;
+                        case 10:
+                            Console.WriteLine("Octubre");
+                            break;
+                        case 11:
+                            Console.WriteLine("Noviembre");
+                            break;
+                        case 12:
+                            Console.WriteLine("Diciembre");
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Dato incorrecto: {dato}");
+                            Console.ResetColor();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"El dato introducido es incorrecto: {dato}");
+                    Console.ResetColor();
+                }
             }
         }
     }
